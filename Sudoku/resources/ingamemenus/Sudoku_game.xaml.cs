b545eaa404cf86bd06482242ed92eabdd49a5314
Csiszár Tamás <csiszar.tamas@verebelyszki.hu>
2022-03-07 07:51:31 +0100

    upd

--- Sudoku/resources/ingamemenus/Sudoku_game.xaml.cs
+++ Sudoku/resources/ingamemenus/Sudoku_game.xaml.cs
@@ -21,6 +21,10 @@ namespace Sudoku
     public partial class Sudoku_game : Window
     {
         public string Felhasznalonev { get; set; }
+        public object LayoutRoot { get; private set; }
+
+        public int[,] grid = new int[9, 9];
+        public string s;
         public Sudoku_game(string felhasznalonev)
         {
             InitializeComponent();
@@ -29,8 +33,82 @@ namespace Sudoku
 
         private void BT_ujjatek_Click(object sender, RoutedEventArgs e)
         {
-            
+            CreateAButton();
         }
+
+        private void CreateAButton()
+        {
+
+        }
+
+        public void Init(ref int[,] grid)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                   grid[i, j] = (i * 3 + i / 3 + j) % 9 + 1;
+
+                }
+            }
+        }
+
+        public void Draw(ref int[,] grid, out string _s)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    s += grid[x, y].ToString() + " ";
+                }
+                s += "\n";
+            }
+            Console.WriteLine(s);
+            _s = s;
+            s = "";
+        }
+
+        //public void ChangeTwoCell(ref int[,] grid, int findValue1, int findValue2)
+        //{
+        //    int xParm1, yParm1, xParm2, yParm2;
+        //    xParm1 = yParm1 = xParm2 = yParm2 = 0;
+        //    for (int i = 0; i < 9; i += 3)
+        //    {
+        //        for (int k = 0; k < 9; k += 3)
+        //        {
+        //            for (int j = 0; j < 3; j++)
+        //            {
+        //                for (int z = 0; z < 3; z++)
+        //                {
+        //                    if (grid[i + j, k + z] == findValue1)
+        //                    {
+        //                        xParm1 = i + j;
+        //                        yParm1 = k + z;
+
+        //                    }
+        //                    if (grid[i + j, k + z] == findValue2)
+        //                    {
+        //                        xParm2 = i + j;
+        //                        yParm2 = k + z;
+
+        //                    }
+        //                }
+        //            }
+        //            grid[xParm1, yParm1] = findValue2;
+        //            grid[xParm2, yParm2] = findValue1;
+        //        }
+        //    }
+        //}
+
+        //public void Update(ref int[,] grid, int shuffleLevel)
+        //{
+        //    for (int repeat = 0; repeat < shuffleLevel; repeat++)
+        //    {
+        //        Random rand = new Random(Guid.NewGuid().GetHashCode());
+        //        Random rand2 = new Random(Guid.NewGuid().GetHashCode());
+        //        ChangeTwoCell(ref grid, rand.Next(1, 9), rand2.Next(1, 9));
+        //    }
+        //}
 
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
