f59865b9db25a1f9e14c4500b42b117689c05e58
Csiszár Tamás <csiszar.tamas@verebelyszki.hu>
2022-03-21 15:24:42 +0100

    upd

--- Sudoku/resources/ingamemenus/Sudoku_game.xaml.cs
+++ Sudoku/resources/ingamemenus/Sudoku_game.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,15 +24,11 @@ namespace Sudoku
     {
         public string Felhasznalonev { get; set; }
         Random rnd = new Random();
-        Random random = new Random();
-        public int[,] grid = new int[9, 9];
-        static string squarevalues;
-        public string szambeiroid;
         public int value;
-        public string content;
+        public int showrandomvalueshints = 45;
 
         List<Button> myButtons = new List<Button>();
-        List<int> myButtonList = new List<int>();
+        SudokuCell[,] cells = new SudokuCell[9, 9];
 
         int currentX = 0;
         int currentY = 0;
@@ -46,8 +43,6 @@ namespace Sudoku
             InitializeComponent();
             Felhasznalonev = felhasznalonev;
         }
-
-        SudokuCell[,] cells = new SudokuCell[9, 9];
         //var wrongCells = new List<SudokuCell>();
 
         private void createCells()
@@ -58,75 +53,75 @@ namespace Sudoku
                 {
                     for (int j = 0; j < 9; j++)
                     {
-                        squarevalues = grid[i, j].ToString();
                         cells[i, j] = new SudokuCell();
-                        cells[i, j].Content = squarevalues;
-                        cells[i, j].Name = "Button" + i.ToString();
+                        cells[i, j].Name = "Cell" + i.ToString();
                         cells[i, j].Width = WIDTH;
                         cells[i, j].Height = HEIGHT;
                         cells[i, j].KeyDown += cell_keyPressed;
+                        cells[i, j].Click += cell_Click;
                         cells[i, j].Background = ((i / 3) + (j / 3)) % 2 == 0 ? new SolidColorBrush(Colors.LightGray) : new SolidColorBrush(Colors.White);
-                        myButtons.Add(cells[i, j]);
+                        //myButtons.Add(cells[i, j]);
                         Canvas.SetLeft(cells[i, j], STEP * currentX);
                         Canvas.SetTop(cells[i, j], STEP * currentY);
                         currentX += WIDTH;
                         MainCanvas.Children.Add(cells[i, j]);
-                        szambeiroid = grid[i, j].ToString();
                     }
                     currentX = 0;
                     currentY += HEIGHT;
+                    isButtonsOnScreen = true;
                 }
             }
             else
             {
+                foreach (var item in cells)
+                {
+                    MainCanvas.Children.Remove(item);
+                }
+                isButtonsOnScreen = false;
+                currentX = 0;
+                currentY = 0;
 
+                createCells();
             }
         }
-
-        private void cell_keyPressed(object sender, KeyEventArgs e)
+        private void cell_Click(object sender, EventArgs e)
         {
-            var cell = sender as SudokuCell;
+            MessageBox.Show("Igen: " +this.Content.ToString());
+        }
+
+        
             // https://playwithcsharpdotnet.blogspot.com/2020/07/develop-sudoku-game-using-basic-csharp-codes.html
-            // Do nothing if the cell is locked
-            if (cell.IsLocked)
-                return;
-
-            int value;
-
-            // Add the pressed key value in the cell only if it is a number
-            if (int.TryParse(e.Key.ToString(), out value))
+            private void cell_keyPressed(object sender, KeyEventArgs e)
             {
-                // Clear the cell value if pressed key is zero
-                if (value == 0)
-                    cell.Clear();
-                else
-                    cell.Content = value.ToString();
+                var cell = sender as SudokuCell;
 
-               // cell.ForeColor = SystemColors.ControlDarkDark;
+                // Do nothing if the cell is locked
+                if (cell.IsLocked)
+                    return;
+
+                int value;
+
+                // Add the pressed key value in the cell only if it is a number
+                if (int.TryParse(e.Key.ToString(), out value))
+                {
+                    // Clear the cell value if pressed key is zero
+                    if (value == 0)
+                        cell.Clear();
+                    else
+                        cell.Content = value.ToString();
+
+                    cell.Foreground = new SolidColorBrush(Colors.DarkGray);
+                }
             }
+            private void cell_teszt(object sender, RoutedEventArgs e)
+        {
+            
         }
         private void startNewGame()
         {
             loadValues();
             //Show values of 45 cells as hint
-            showRandomValuesHints(45);
-        }
-
-        private void showRandomValuesHints(int hintsCount)
-        {
-            // Show value in random cells
-            // The hints count is based on the level player choose
-            for (int i = 0; i < hintsCount; i++)
-            {
-                var rX = random.Next(9);
-                var rY = random.Next(9);
-
-                // Style the hint cells differently and
-                // lock the cell so that player can't edit the value
-                cells[rX, rY].Content = cells[rX, rY].Value.ToString();
-                //cells[rX, rY].ForeColor = Color.Black;
-                cells[rX, rY].IsLocked = true;
-            }
+            showRandomValuesHints(showrandomvalueshints);
         }
 
         private void loadValues()
@@ -172,7 +167,7 @@ namespace Sudoku
                 }
 
                 // Take a random number from the numbers left in the list
-                value = numsLeft[random.Next(0, numsLeft.Count)];
+                value = numsLeft[rnd.Next(0, numsLeft.Count)];
                 cells[i, j].Value = value;
 
                 // Remove the allocated value from the list
@@ -211,165 +206,33 @@ namespace Sudoku
 
             return true;
         }
+
+        private void showRandomValuesHints(int hintsCount)
+        {
+            // Show value in random cells
+            // The hints count is based on the level player choose
+            for (int i = 0; i < hintsCount; i++)
+            {
+                var rX = rnd.Next(9);
+                var rY = rnd.Next(9);
+
+                // Style the hint cells differently and
+                // lock the cell so that player can't edit the value
+                cells[rX, rY].Content = cells[rX, rY].Value.ToString();
+                cells[rX, rY].Foreground = new SolidColorBrush(Colors.Red);
+                cells[rX, rY].IsLocked = true;
+            }
+        }
+
+
         private void BT_ujjatek_Click(object sender, RoutedEventArgs e)
         {
             createCells();
             startNewGame();
-            //squarevalues = "";
-            //Init(ref grid);
-            //Update(ref grid, 10);
-            //GenerateButtons(ref grid,squarevalues);
-            //RemoveButtons();
         }
-        /*
-        private void RemoveButtons()
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    
-                }
-            }
-        }
-
-        static void Init(ref int[,] grid)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    grid[i, j] = (i * 3 + i / 3 + j) % 9 + 1;
-
-                }
-            }
-        }*/
 
         
 
-        private void BT_remove_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (var item in cells)
-            {
-                MainCanvas.Children.Remove(item);
-            }
-            isButtonsOnScreen = false;
-            currentX = 0;
-            currentY = 0;
-            
-        }
-
-        /*
-
-        private void GenerateButtons(ref int[,] grid, string _s)
-        {
-            
-            if (!isButtonsOnScreen)
-            {
-                Createbuttons(ref grid, squarevalues);
-            }
-            else
-            {
-                foreach (var item in myButtons)
-                {
-                    MainCanvas.Children.Remove(item);
-                }
-                isButtonsOnScreen = false;
-                currentX = 0;
-                currentY = 0;
-                Createbuttons(ref grid, squarevalues);
-            }
-        }
-
-        private void Createbuttons(ref int[,] grid, string _s)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    squarevalues = grid[i,j].ToString();
-                    //myButtonList[,] =;
-                    int num = rnd.Next(100);
-                    if (num < 30)
-                    {
-                        //squarevalues = "";
-                    }
-                    Button newBtn = new Button();
-                    //newBtn.Content = i.ToString();
-                    newBtn.Content = squarevalues;
-                    newBtn.Name = "Button" + i.ToString();
-                    newBtn.Width = WIDTH;
-                    newBtn.Height = HEIGHT;
-                    newBtn.Click += Interakcio;
-                    myButtons.Add(newBtn);
-                    Canvas.SetLeft(newBtn, STEP * currentX);
-                    Canvas.SetTop(newBtn, STEP * currentY);
-                    currentX += WIDTH;
-                    MainCanvas.Children.Add(newBtn);
-                    szambeiroid = grid[i,j].ToString();
-                }
-                currentX = 0;
-                currentY += HEIGHT;
-            }
-            _s = squarevalues;
-            //squarevalues = "";
-            isButtonsOnScreen = true;
-            
-        }
-
-        private void Interakcio(object sender, RoutedEventArgs e)
-        {
-            int id = Convert.ToInt32(szambeiroid);
-            var frm = new SzamBeiro(id);
-            MessageBox.Show(""+id);
-            //frm.ShowDialog();
-
-            //MessageBox.Show(squarename);
-        }
-
-        private void Update(ref int[,] grid, int shuffleLevel)
-        {
-            for (int repeat = 0; repeat < shuffleLevel; repeat++)
-            {
-                Random rand = new Random(Guid.NewGuid().GetHashCode());
-                Random rand2 = new Random(Guid.NewGuid().GetHashCode());
-                ChangeTwoCell(ref grid, rand.Next(1, 9), rand2.Next(1, 9));
-            }
-        }
-
-        private void ChangeTwoCell(ref int[,] grid, int findValue1, int findValue2)
-        {
-            int xParm1, yParm1, xParm2, yParm2;
-            xParm1 = yParm1 = xParm2 = yParm2 = 0;
-            for (int i = 0; i < 9; i += 3)
-            {
-                for (int k = 0; k < 9; k += 3)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        for (int z = 0; z < 3; z++)
-                        {
-                            if (grid[i + j, k + z] == findValue1)
-                            {
-                                xParm1 = i + j;
-                                yParm1 = k + z;
-
-                            }
-                            if (grid[i + j, k + z] == findValue2)
-                            {
-                                xParm2 = i + j;
-                                yParm2 = k + z;
-
-                            }
-                        }
-                    }
-                    grid[xParm1, yParm1] = findValue2;
-                    grid[xParm2, yParm2] = findValue1;
-                }
-            }
-        }
-
-        */
 
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
@@ -385,6 +248,11 @@ namespace Sudoku
         private void BT_beallitasok_Click(object sender, RoutedEventArgs e)
         {
             new Beallitasokmenu(Felhasznalonev).Show();
+        }
+
+        private void BT_ujjatek_GotFocus(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
